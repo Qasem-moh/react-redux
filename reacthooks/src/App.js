52d3925d04131a7776c080dbb0733d9703953b56
qasem-moh <qasemcoderjo2020@gmail.com>
2021-11-30 01:34:54 +0200

    app

--- reacthooks/src/App.js
+++ reacthooks/src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { LoginCl, LoginFu } from "./LoginForm";
 
 function App() {
   return (
     <div>
       <h2>Hello</h2>
+      <LoginFu />
+      <LoginCl />
     </div>
   )
 }
