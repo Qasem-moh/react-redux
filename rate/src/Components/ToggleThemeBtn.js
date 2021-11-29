d937f2f47f0770c59fb83e3e2c71ea110421ed17
qasem-moh <qasemcoderjo2020@gmail.com>
2021-11-30 00:51:40 +0200

    app

--- rate/src/Components/ToggleThemeBtn.js
+++ rate/src/Components/ToggleThemeBtn.js
@@ -1,18 +1,26 @@
 import React from "react";
 import ThemeContext from "../Context/ThemeContext";
-function ToggleThemeBtn(props) {
-    return (
-        <div>
-            <ThemeContext.Consumer>
-                {({ theme }) => <>
-                    <button onClick={props.toggleTheme}>
-                        Toggle theme to
-                    </button>
-                </>
-                }
-            </ThemeContext.Consumer>
-        </div>
-    );
+class ToggleThemeBtn extends React.Component {
+    componentDidCatch() {
+        console.log(theme);
+    }
+    render() {
+        return (
+            <div>
+                <ThemeContext.Consumer>
+                    {({ theme, toggleTheme }) => <>
+                        <button onClick={toggleTheme}>
+                            Toggle theme to {theme === "dark" ? "white" : "dark"}
+                        </button>
+                    </>
+                    }
+                </ThemeContext.Consumer>
+            </div>
+        );
+
+    }
 }
+
+ToggleThemeBtn.contextType = ThemeContext;
 
 export default ToggleThemeBtn;
