1b725caf3331b94a88a773706163d414c6ba7fa7
qasem-moh <qasemcoderjo2020@gmail.com>
2021-12-04 22:26:51 +0200

    app

--- reacthooks/src/App.js
+++ reacthooks/src/App.js
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { LoginCl, LoginFu } from "./LoginForm";
 
 function App() {
+  const [visible, setVisible] = useState(true)
   return (
     <div>
-      <h2>Hello</h2>
-      <LoginCl />
-      <LoginFu />
+      {visible ? <>
+        <h2>Hello</h2>
+        <LoginCl />
+        <LoginFu /></> : null
+
+      }
+
+      <button onClick={() => setVisible(!visible)}>Toogle</button>
     </div>
   )
 }
