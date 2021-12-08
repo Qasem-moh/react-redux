8b8bb134d1bbade4e8dbaadf61a35b64c41813ef
qasem-moh <qasemcoderjo2020@gmail.com>
2021-12-09 00:48:20 +0200

    app

--- single_pageapp_lication/src/Header.js
+++ single_pageapp_lication/src/Header.js
@@ -1,4 +1,4 @@
-import React, { useContext} from 'react'
+import React, { useContext } from 'react'
 import { AuthContext } from './AuthContext'
 
 
@@ -7,14 +7,20 @@ import { AuthContext } from './AuthContext'
 export default function Header() {
     const authContext = useContext(AuthContext)
 
+    function logout() {
+        localStorage.removeItem('token')
+        localStorage.removeItem('email')
+        authContext.setAuth({})
+    }
+
     return (
         <nav className="navbar navbar-light bg-light mt-2 mb-2">
             <div className="container-fluid">
                 <span className="navbar-brand mb-0 h1">React Hooks</span>
                 <div>{authContext.auth.email ? <div>
                     {authContext.auth.email}
-                    { " "}
-                    <button className="btn btn-danger btn-sm ">Logout</button>
+                    {" "}
+                    <button className="btn btn-danger btn-sm " onClick={logout}>Logout</button>
                 </div> : "you need login"}</div>
             </div>
         </nav>
