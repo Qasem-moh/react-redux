5874beafb07a3d954959d878e4524ae94819217d
qasem-moh <qasemcoderjo2020@gmail.com>
2021-12-09 11:17:50 +0200

    app

--- userefrance/src/Components/FunctionComponents.js
+++ userefrance/src/Components/FunctionComponents.js
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 
 
 export default function FunctionComponents() {
+
+    const [fName, setFName] = useState("")
+    const [lName, setLName] = useState('')
+    function run() {
+
+    }
     return (
         <div>
             Hi from function<br />
-            <input placeholder="First name" />
-            <input placeholder="last name" />
+            <input placeholder="First name" value={fName} onClick={(e) => setFName(e.target.value)} />
+            <input placeholder="last name" value={lName} onClick={(e) => setLName(e.target.value)} />
             <button>Save</button>
         </div>
     )
