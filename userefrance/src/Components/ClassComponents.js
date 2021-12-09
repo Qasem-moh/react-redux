dc90297a25aa0de266b04cc3d8d49af4e333f173
qasem-moh <qasemcoderjo2020@gmail.com>
2021-12-09 10:58:22 +0200

    app

--- userefrance/src/Components/ClassComponents.js
+++ userefrance/src/Components/ClassComponents.js
@@ -1,1 +1,11 @@
 import React from 'react'
+
+export default class ClassComponents extends React.Component {
+    render() {
+        return (
+            <div>
+                Hi from clsass
+            </div>
+        )
+    }
+}
