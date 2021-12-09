f19aee855837b31c096e4cf11be9f7323fef3f21
qasem-moh <qasemcoderjo2020@gmail.com>
2021-12-09 11:11:45 +0200

    app

--- userefrance/src/App.js
+++ userefrance/src/App.js
@@ -1,9 +1,11 @@
 import ClassComponents from "./Components/ClassComponents";
+import FunctionComponents from "./Components/FunctionComponents";
 
 function App() {
   return (
     <div className="App">
       <ClassComponents />
+      <FunctionComponents/>
     </div>
   );
 }
--- userefrance/src/Components/FunctionComponents.js
+++ userefrance/src/Components/FunctionComponents.js
@@ -1,1 +1,14 @@
 import React from 'react'
+
+
+
+export default function FunctionComponents() {
+    return (
+        <div>
+            Hi from function<br />
+            <input placeholder="First name" />
+            <input placeholder="last name" />
+            <button>Save</button>
+        </div>
+    )
+}
