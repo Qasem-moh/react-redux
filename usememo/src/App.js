e221b35b34ada961a27843291a696fe167b06b3d
qasem-moh <qasemcoderjo2020@gmail.com>
2021-12-11 02:56:00 +0200

    app

--- usememo/src/App.js
+++ usememo/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useMemo } from 'react'
+
+const randomeColor = () => '#' + (Math.random() * 0xFFFFFF << 0).toString(16);
 
 function App() {
+
+  const [fistName, setFirstName] = useState('')
+  const [lastName, setLastName] = useState('')
+  const [age, SetAge] = useState('')
+
+  const fullName = useMemo(() => <div style={{ color: randomeColor() }}>{fistName}{lastName}</div>, [fistName, lastName])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      fullName: {fullName}
+      age:{age}
+      <br />
+      <input value={fistName} placeholder='first name' onChange={(e => setFirstName(e.target.value))} />
+      <input value={lastName} placeholder='last name' onChange={(e => setLastName(e.target.value))} />
+      <input value={age} placeholder='age' onChange={(e => SetAge(e.target.value))} />
     </div>
   );
 }
